26517d9440129298c7cd8bc676fd5e5205f79996
vdolhushyn <vetajika@gmail.com>
2023-08-12 19:41:26 +0300

    2. add two numbers

---
+++ src/add_two_numbers.rs
@@ -0,0 +1,77 @@
+use crate::list_node::ListNode;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        return match (l1, l2) {
+            (None, None) => {
+                None
+            }
+            (Some(v), None) | (None, Some(v)) => {
+                Some(v)
+            }
+            (Some(f), Some(s)) => {
+                let sum = f.val + s.val;
+                if sum >= 10 {
+                    let carry = Option::from(Box::new(ListNode::new(sum / 10)));
+                    Option::from(Box::new(ListNode {
+                        val: sum - 10,
+                        next: Solution::add_two_numbers(Solution::add_two_numbers(carry, f.next), s.next),
+                    }))
+                } else {
+                    Option::from(Box::new(ListNode {
+                        val: sum,
+                        next: Solution::add_two_numbers(f.next, s.next),
+                    }))
+                }
+            }
+        };
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_add_each_node_of_list() {
+        let first_list = Some(Box::new(ListNode {
+            val: 9,
+            next: Some(Box::new(ListNode {
+                val: 9,
+                next: Some(Box::new(ListNode {
+                    val: 9,
+                    next: None
+                })),
+            })),
+        }));
+
+        let second_list = Some(Box::new(ListNode {
+            val: 9,
+            next: Some(Box::new(ListNode {
+                val: 9,
+                next: Some(Box::new(ListNode {
+                    val: 9,
+                    next: None
+                })),
+            })),
+        }));
+
+        let expected = Some(Box::new(ListNode {
+            val: 8,
+            next: Some(Box::new(ListNode {
+                val: 9,
+                next: Some(Box::new(ListNode {
+                    val: 9,
+                    next: Some(Box::new(ListNode {
+                        val: 1,
+                        next: None
+                    }))
+                })),
+            })),
+        }));
+
+        assert_eq!(Solution::add_two_numbers(first_list, second_list), expected)
+    }
+}
